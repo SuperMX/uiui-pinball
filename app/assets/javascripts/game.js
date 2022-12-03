170a795f765168dcd200a882bb3af78af4161c26
lukas.dietrich <lukas.dietrich@danuvius-consulting.at>
2022-12-03 14:12:59 +0100

    added buttons for mobile version

--- app/assets/javascripts/game.js
+++ app/assets/javascripts/game.js
@@ -4,9 +4,21 @@ import { createBall } from './ball';
 export const launch = function launch() {
   let launched = false;
 
+	document.addEventListener("touchstart", function touchstart(e) {
+		var x = e.touches.length;
+		if (x === 3) {
+		let pinball = createBall();
+		Matter.Body.setPosition(pinball, { x: 525, y: 650 });
+		Matter.Body.setVelocity(pinball, {x: 0, y: 10 });
+    }
+	}
+	);
+
   document.addEventListener("keydown", function keyDown(e) {
     let keyCode = e.keyCode;
-    if (keyCode === 38 || keyCode === 32) {
+	var x = e.touches[0].clientX;
+	var y = e.touches[0].clientY;
+    if (keyCode === 38 || keyCode === 32 || x === 3) {
       let pinball = createBall();
       Matter.Body.setPosition(pinball, { x: 525, y: 650 });
       Matter.Body.setVelocity(pinball, {x: 0, y: 10 });
